3541a93b994312aca9e1cf4c18117a3152e557e0
jphorejsi <jphorejsi@gmail.com>
2023-12-21 17:59:16 -0600

    first commit

---
+++ scripts/Cube_Right.gd
@@ -0,0 +1,53 @@
+extends Node3D
+
+@onready var cube_scene = preload("res://objects/blocks/blue/bluerightblock.tscn")
+@onready var cube_scene_r = preload("res://objects/blocks/red/redrightblock.tscn")
+
+# @onready var player = get_node("/root/XROrigin3D")
+@onready var player2 = $%XROrigin3D as XROrigin3D
+  # Replace with the actual path to your player node
+# var cubes = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# var cube_instance = spawn_cube()
+	# cubes.append(cube_instance)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	print(player2.position)
+	# print(cubes.size())
+	var rand = randf()
+	
+	if rand < 0.01:
+		var cube_instance = spawn_cube(cube_scene_r)
+		cube_instance.add_to_group("reds")
+		# cubes.append(cube_instance)
+	if rand >= 0.01 and rand < 0.02:
+		var cube_instance = spawn_cube(cube_scene)
+		cube_instance.add_to_group("blues")
+
+	for cube_instance in self.get_children():
+		move_cube(cube_instance,delta)
+		# print(cube_instance.position)	
+
+func spawn_cube(cube):
+	var cube_instance = cube.instantiate()
+	self.add_child(cube_instance)
+	cube_instance.transform.origin = self.transform.origin
+	return cube_instance
+
+
+
+
+func move_cube(cube_instance, delta):
+	if cube_instance:
+		if cube_instance.transform.origin.z >= 6.0:
+			# cubes.erase(cubes.find(cube_instance))
+			cube_instance.queue_free()
+		elif not cube_instance.is_queued_for_deletion():
+			cube_instance.position.z += 0.05
+		# Move the cube towards the player in the process function
+		# cube_instance.translate(Vector3(0, 0, 1 * speed * delta))
---
+++ scripts/rightsaber.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+var K : float = 2.0 # Adjust this value (0 < k < 1) to control the non-linearity
+var D : float = 0.1 # The threshold distance for linear mapping
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var xr_controller = get_parent() # Reference to the XR controller node
+	var XRCamera_position = %XRCamera3D.global_position
+	
+	# Get the position of the XR controller
+	var controller_position : Vector3 = xr_controller.global_position
+
+	# Calculate the distance from the origin to the controller position
+	var distance : float = (controller_position - XRCamera_position).length()
+
+	# Apply the non-linear mapping function
+	var mapped_distance : float = non_linear_map(distance)
+
+	# Normalize the controller position vector
+	var normalized_position : Vector3 = (controller_position - XRCamera_position).normalized()
+
+	# Calculate the position of the LeftSaber based on the mapped distance
+	global_position = XRCamera_position + normalized_position * mapped_distance
+	
+	var redsgroup = get_tree().get_nodes_in_group("reds")
+	for object in redsgroup:
+		var correct = object.find_child("CorrectArea3D") as Area3D
+		var incorrect = object.find_child("MissArea3D") as Area3D
+		var saber = self.find_child("Area3D") as Area3D
+		if correct.overlaps_area(saber):
+			object.queue_free()
+	
+func non_linear_map(distance: float) -> float:
+	if distance < D:
+		return distance
+	else:
+		return distance + K * pow(distance - D, 2)
